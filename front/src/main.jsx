0f0913423cc3a24e4ee6e72a03ad0060bd1f7ec0
Gabriel Carneiro <94560606+GabrielShepp@users.noreply.github.com>
2023-11-23 19:48:56 -0300

    Criação dos arquivos de layout
    Criação dos arquivos de layout dos seguintes layouts: assessments, avaliation, description, home, login, register. Todos com seus respectivos arquivos .jsx e outra pasta com todos os arquivos .css dos layouts citados

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from  'react-router-dom'
 import App from './App.jsx'
+import Login from './login/login.jsx'
+import Register from './register/register.jsx'
 import './index.css'
+
+const router =  createBrowserRouter([
+  {
+    path:'/login',
+    element: <Login/>
+  },
+  {
+    path:'/register',
+    element: <Register/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- front/src/register/register.jsx
+++ front/src/register/register.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import '../style/Login.css'
 
-function App() {
-  return (
-   <form action="">
+export default function Login(){
+    return(
+    <form action="">
     <img src="https://www.ubuy.com.br/productimg/?image=aHR0cHM6Ly9pNS53YWxtYXJ0aW1hZ2VzLmNvbS9zZW8vRnJlc2gtU2xpY2luZy1Ub21hdG8tRWFjaF9hMWU4ZTQ0YS0yYjgyLTQ4YWItOWMwOS1iNjg0MjBmNjk1NGMuMDRmNmUwZTg3ODA3ZmM1NDU3ZjU3ZTNlYzA3NzAwNjEuanBlZw.jpg" alt="Imagem teste" />
     <label htmlFor="email">Email</label>
     <input type="text" name='email' />
     <label htmlFor="senha">Senha</label>
     <input type="text" name='senha' />
+    <label htmlFor="conf_senha">Confirmar Senha</label>
+    <input type="text" name='conf_senha' />
     <button>LOGIN</button>
-   </form>
-  )
+   </form>)
 }
-
-export default App
