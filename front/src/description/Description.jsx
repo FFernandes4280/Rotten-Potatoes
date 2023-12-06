0cc91b60fff0e652d88049923adcd4e75f228a44
Gabriel Carneiro <94560606+GabrielShepp@users.noreply.github.com>
2023-12-06 13:34:21 -0300

    Estilização inicial da description
    Estilização do posicionamento dos itens no layout description

--- front/src/description/Description.jsx
+++ front/src/description/Description.jsx
@@ -8,18 +8,26 @@ export default function Description(){
             <button>LOGIN</button>
         </div>
         <div>
-            <img src="https://www.intoxianime.com/wp-content/uploads/2023/08/F4M7DiMbgAAuHUV.jpg" alt="Poster" />
+            <div className='cartaz'>
+                <img src="https://www.intoxianime.com/wp-content/uploads/2023/08/F4M7DiMbgAAuHUV.jpg" alt="Poster" />
+            </div>
+            <div className='Avaliacao'>
+                <img src="https://pixel.cuboup.com/wp-content/uploads/edd/2022/05/Icone-Joinha-Like.jpg" alt="avaliaçãocritico" />
+                <img src="https://pixel.cuboup.com/wp-content/uploads/edd/2022/05/Icone-Joinha-Like.jpg" alt="publico" />
+            </div>
+            <div className='Sinopse'>
+                <h2>Sinopse:</h2>
+                <p>A maga élfica Frieren e seus bravos companheiros aventureiros derrotaram o Rei Demônio e trouxeram paz à terra. 
+                    Com a grande luta terminada, todos eles seguem caminhos separados para viver uma vida tranquila. 
+                    Mas como uma elfo, a quase imortal Frieren durará muito mais que o resto de seu antigo bando.
+Como ela aceitará a mortalidade de seus amigos? Como ela pode encontrar satisfação em sua própria vida e pode aprender a entender o que a vida significa para os humanos ao seu redor? Frieren inicia uma nova jornada para encontrar a resposta.</p>
+            </div>
+            <div className='trailer'>
+                <h1>Trailer</h1>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Iwr1aLEDpe4?si=n5iE7mbgCF3pRUaF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
         </div>
-        <div>
-            <img src="https://pixel.cuboup.com/wp-content/uploads/edd/2022/05/Icone-Joinha-Like.jpg" alt="avaliaçãocritico" />
-            <img src="https://pixel.cuboup.com/wp-content/uploads/edd/2022/05/Icone-Joinha-Like.jpg" alt="publico" />
-        </div>
-        <div>
-            <p>Sinopse</p>
-        </div>
-        <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/Iwr1aLEDpe4?si=n5iE7mbgCF3pRUaF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
+       
      </>
     )
 }
--- front/src/home/Home.jsx
+++ front/src/home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home(){
         <>
         <div className='barrasup'>
             <img src="https://www.ubuy.com.br/productimg/?image=aHR0cHM6Ly9pNS53YWxtYXJ0aW1hZ2VzLmNvbS9zZW8vRnJlc2gtU2xpY2luZy1Ub21hdG8tRWFjaF9hMWU4ZTQ0YS0yYjgyLTQ4YWItOWMwOS1iNjg0MjBmNjk1NGMuMDRmNmUwZTg3ODA3ZmM1NDU3ZjU3ZTNlYzA3NzAwNjEuanBlZw.jpg" alt="Logo" />
-            <button>LOGIN</button>
+            <button name='LoginButton'>LOGIN</button>
         </div>
         <div className='carosel'>
             <h2>Filmes</h2>
