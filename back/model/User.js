54887d03ec69434f2cd3c099a6051f70386e9003
FFernandes4280 <d2022002088@unifei.edu.br>
2023-12-11 16:02:15 -0300

    db.json

---
+++ back/model/User.js
@@ -0,0 +1,10 @@
+class User {
+
+    constructor(id, username, email,password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+  }
+  
+  module.exports = User;
