adaa1429353d530318c4d94afa9b83e41f4d2bbf
Martins <104658309+MartinesEmanuel@users.noreply.github.com>
2023-12-12 19:46:11 -0300

    Update randomfilmes.jsx
    COM AXIOS

--- front/src/Caroseu/redirecionamento/randomfilmes.jsx
+++ front/src/Caroseu/redirecionamento/randomfilmes.jsx
@@ -1,39 +1,29 @@
 async function RandomMovie(genre) {
-    if (genre === 'rand') {
-      // Se o gênero for 'rand', retorna qualquer filme aleatório
-      const ApiURL = 'http://www.omdbapi.com/?apikey=YOUR_OMDB_API_KEY&type=movie';
-      return fetch(ApiURL)
-        .then(response => response.json())
-          .then(data=>{
-              // Verifique se a solicitação foi bem-sucedida
-              if (data.Response === 'True') {
-                // Retorna o URL do poster
-                return data.Poster, data.Title;
+  let ApiURL;
 
-              } else {
-                return null; // Retorna nulo se o filme não for encontrado
-              }
-
-          })
-
-    } else {
-      // Caso contrário, retorna um filme aleatório do gênero especificado
-      const response = await axios.get(`http://www.omdbapi.com/?apikey=YOUR_OMDB_API_KEY&type=movie&genre=${genre}`);
-      const randomIndex = Math.floor(Math.random() * response.data.Search.length);
-      const imdbID = response.data.Search[randomIndex].imdbID;
-      const ApiURLtoRandomGenre =  `http://www.omdbapi.com/?apikey=YOUR_OMDB_API_KEY&i=${imdbID}`;
-      return fetch(ApiURLtoRandomGenre)
-        .then(response => response.json())
-          .then(data=>{
-              // Verifique se a solicitação foi bem-sucedida
-              if (data.Response === 'True') {
-                // Retorna o URL do poster
-                return data.Poster, data.Title;
-
-              } else {
-                return null; // Retorna nulo se o filme não for encontrado
-              }
-
-          })
-    }
+  if (genre === 'rand') {
+    ApiURL = 'http://www.omdbapi.com/?apikey=YOUR_OMDB_API_KEY&type=movie';
+  } else {
+    const response = await axios.get(`http://www.omdbapi.com/?apikey=YOUR_OMDB_API_KEY&type=movie&genre=${genre}`);
+    const randomIndex = Math.floor(Math.random() * response.data.Search.length);
+    const imdbID = response.data.Search[randomIndex].imdbID;
+    ApiURL = `http://www.omdbapi.com/?apikey=YOUR_OMDB_API_KEY&i=${imdbID}`;
   }
+
+  try {
+    const response = await fetch(ApiURL);
+    const data = await response.json();
+
+    // Verifique se a solicitação foi bem-sucedida
+    if (data.Response === 'True') {
+      // Retorna um objeto com o URL do poster e o título
+      return { poster: data.Poster, title: data.Title };
+    } else {
+      // Retorna null se o filme não for encontrado
+      return null;
+    }
+  } catch (error) {
+    console.error('Erro na solicitação da API:', error);
+    return null;
+  }
+}
