f07162b75468b3a5c82fe00c1ca4d4176f317306
Martins <104658309+MartinesEmanuel@users.noreply.github.com>
2023-12-12 18:25:45 -0300

    API FUNCIONANDO PAPAI

--- front/src/Caroseu/redirecionamento/randomfilmes.jsx
+++ front/src/Caroseu/redirecionamento/randomfilmes.jsx
@@ -1,30 +1,19 @@
-const TMDB_API_KEY = '043fe6a0cc6d215f5b63dc8fb46878b2I'; // Substitua pela sua chave de API do TMDb
-const BASE_URL = 'https://api.themoviedb.org/3';
+import {useState,useEffect} from "react";
+const imageUrl = import.meta.env.VITE_IMG;
+const apiKey = import.meta.env.VITE_API_KEY;
 
-async function obterFilmesPopulares() {
-  try {
-    const response = await axios.get(`${BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`);
-    const filmes = response.data.results.map(filme => ({
-      poster: `https://image.tmdb.org/t/p/w500${filme.poster_path}`,
-      titulo: filme.original_title,
-    }));
-
-    return filmes;
-  } catch (error) {
-    console.error('Erro ao obter filmes populares:', error.message);
-    throw error;
+const Home = ( )=> {
+  const [topMovies,setTopMovies] = useState([]);
+  
+  const getTopRatedMovies = async(url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
   }
-}
-}
 
-// // Exemplo de uso:
-// getRandomMoviePosterAndTitle('rand')
-//   .then(movie => {
-//     if (movie) {
-//       console.log('Título do Filme:', movie.title);
-//       console.log('URL do Poster:', movie.posterUrl);
-//     } else {
-//       console.log('Não foi possível obter um filme aleatório.');
-//     }
-//   })
-//   .catch(error => console.error('Erro:', error));
+  useEffect(()=>{
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}` ;
+    getTopRatedMovies(topRatedUrl)
+  },[])
+
+} ;
