7760bb7b9404f94a2b0d3308f344adec37fcbea6
Martins <104658309+MartinesEmanuel@users.noreply.github.com>
2023-12-14 17:46:41 -0300

    Teste_Envio_avaliação back

--- front/src/avaliation/Avaliation.jsx
+++ front/src/avaliation/Avaliation.jsx
@@ -1,107 +1,114 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
 import '../style/Avaliation.css';
 
 const Avaliation = () => {
-    const { id } = useParams();
-    console.log(id);
+  const { id } = useParams();
+  const [movieData, setMovieData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [text, setText] = useState('');
+  const [nota, setNota] = useState(1);
 
-    const [movieData, setMovieData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [formData, setFormData] = useState({
-        textava: '',
-        Nota: '1',
-    });
-
-    useEffect(() => {
-        const fetchMovieData = async () => {
-            try {
-                const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=043fe6a0cc6d215f5b63dc8fb46878b2`);
-                if (!response.ok) {
-                    throw new Error('Erro ao obter os dados do filme');
-                }
-                const result = await response.json();
-                setMovieData(result);
-                setLoading(false);
-            } catch (error) {
-                console.error('Erro ao obter os dados do filme:', error.message);
-                setError('Erro ao obter dados do filme. Tente novamente mais tarde.');
-                setLoading(false);
-            }
-        };
-        fetchMovieData();
-    }, [id]);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Lógica para tratar o envio do formulário
-        console.log('Texto:', formData.textava);
-        console.log('Nota:', formData.Nota);
-        // Implemente a lógica de avaliação ou envio para o servidor conforme necessário.
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=043fe6a0cc6d215f5b63dc8fb46878b2`);
+        if (!response.ok) {
+          throw new Error('Erro ao obter os dados do filme');
+        }
+        const result = await response.json();
+        setMovieData(result);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erro ao obter os dados do filme:', error.message);
+        setError('Erro ao obter dados do filme. Tente novamente mais tarde.');
+        setLoading(false);
+      }
     };
 
-    const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value,
-        });
-    };
+    fetchMovieData();
+  }, [id]);
 
-    if (loading) {
-        return <p>Carregando...</p>;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Substitua a URL abaixo pela URL do seu endpoint de backend
+      const apiUrl = 'https://seu-backend.com/api/avaliacoes';
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id_filme: id,
+          texto: text,
+          nota: nota,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao enviar avaliação para o backend');
+      }
+
+      // Implemente qualquer lógica adicional que você precisar após o envio bem-sucedido
+
+    } catch (error) {
+      console.error('Erro ao enviar avaliação para o backend:', error.message);
     }
+  };
 
-    if (error) {
-        return <p>{error}</p>;
-    }
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
-    const { title, overview } = movieData;
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit} noValidate>
-                <div className='corpoava'>
-                    <div className='postertitulo'>
-                        <img src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`} alt={title} />
-                        <h2>{title}</h2>
-                    </div>
-                    <div className='avaliação'>
-                        <label htmlFor="avaliacao">Avaliação:</label>
-                        <textarea
-                            name="textava"
-                            id="avali"
-                            cols="30"
-                            rows="10"
-                            value={formData.textava}
-                            onChange={handleChange}
-                        ></textarea>
-                        <div className='divbot'>
-                            <label htmlFor="nota">Nota:</label>
-                            <select
-                                name="Nota"
-                                id="nota"
-                                value={formData.Nota}
-                                onChange={handleChange}
-                            >
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                            </select>
-                            <button type="submit">AVALIAR</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </>
-    );
-}
+  const { title } = movieData;
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className='corpoava'>
+          <div className='postertitulo'>
+            <img src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`} alt={title} />
+            <h2>{title}</h2>
+          </div>
+          <div className='avaliacao'>
+            <label htmlFor="avaliacao">Avaliação:</label>
+            <textarea
+              name="textava"
+              id="avali"
+              cols="30"
+              rows="10"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            ></textarea>
+            <div className='divbot'>
+              <select
+                name="Nota"
+                id="nota"
+                value={nota}
+                onChange={(e) => setNota(Number(e.target.value))}
+              >
+                {/* Opções de 1 a 10 para a nota */}
+                {Array.from({ length: 10 }, (_, index) => index + 1).map((value) => (
+                  <option key={value} value={value}>
+                    {value}
+                  </option>
+                ))}
+              </select>
+              <button type="submit">AVALIAR</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default Avaliation;
